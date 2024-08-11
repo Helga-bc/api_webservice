7e62d484745804c7dbcce67031a09b8faae750ed
Olga Blashko <126601252+Helga-bc@users.noreply.github.com>
2024-08-11 21:17:16 +0300

    update urls

--- webapi/urls.py
+++ webapi/urls.py
@@ -20,6 +20,7 @@ from api import urls as api_urls
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('admin/', admin.site.urls),
     path('api/', include(api_urls)),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
